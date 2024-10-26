f36b3a14df2030b836c1ce87da12805a87b7c6e5
Marina <marna.li@outlook.com>
2024-10-26 14:40:44 +0200

    Toggle NSwag

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -11,8 +11,9 @@ builder.Services.AddControllers();
 
 builder.Services.AddHttpContextAccessor();
 
-//builder.Services.AddSwaggerDocument(settings => settings.SchemaSettings.SchemaType = NJsonSchema.SchemaType.OpenApi3);
+builder.Services.AddSwaggerDocument(settings => settings.SchemaSettings.SchemaType = NJsonSchema.SchemaType.OpenApi3);
 
+/*
 builder.Services.AddOpenApi(options =>
             {
                 options.ApplyServersTransformer();
@@ -22,6 +23,7 @@ builder.Services.AddOpenApi(options =>
                 options.ApplySchemaNameTransforms(TypeExtensions.GetSchemaName);
                 // options.ApplyInheritanceTransformer();
             });
+*/
 
 var app = builder.Build();
 
@@ -29,10 +31,10 @@ app.UseHttpsRedirection();
 
 app.UseRouting();
 
-//app.MapOpenApi();
-app.MapOpenApiYaml();
+//// app.MapOpenApi();
+// app.MapOpenApiYaml();
 
-//app.UseOpenApi(x => x.Path = "/openapi/{documentName}.yaml");
+app.UseOpenApi(x => x.Path = "/openapi/{documentName}.yaml");
 
 if (app.Environment.IsDevelopment())
 {
